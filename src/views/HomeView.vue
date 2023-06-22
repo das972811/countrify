44fedc70a9bd734b3da9fee702001825c16b4990
das972811 <salasdiego54@gmail.com>
2023-06-21 23:43:13 -0500

    Initial Code Layout

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+<template>
+    <main class="container">
+        <section>
+            <h1>Where in the world?</h1>
+            <p>Dark Mode</p>
+        </section>
+    </main>
+</template>
+<style scoped>
+</style>
