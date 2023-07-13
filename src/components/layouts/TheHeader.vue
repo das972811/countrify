64cb124f5bc383468827580f089a8b0ff5936416
das972811 <salasdiego54@gmail.com>
2023-07-13 16:20:05 -0500

    Mionr

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -2,11 +2,17 @@
     <header>
         <h1>Where in the World?</h1>
         <div>
-            <h3>Dark Mode</h3>
+            <button @click="changeColorTheme">Dark Mode</button>
         </div>
     </header>
 </template>
 <script setup lang="ts">
+
+
+function changeColorTheme()
+{
+    console.log("Diego is awesome at programming");
+}
 
 </script>
 <style scoped>
