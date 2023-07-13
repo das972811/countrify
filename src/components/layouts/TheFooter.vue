7e5b52247b1481d71f61227a2f6861252a26f91e
das972811 <salasdiego54@gmail.com>
2023-07-13 15:55:04 -0500

    Added a new Footer component and created a sticky footer

---
+++ src/components/layouts/TheFooter.vue
@@ -0,0 +1,10 @@
+<template>
+    <footer>
+        <h2>Diego was here</h2>
+    </footer>
+</template>
+<style scoped>
+    footer {
+        background: green;
+    }
+</style>
--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -1,10 +1,16 @@
 <template>
     <header>
         <h1>Where in the World?</h1>
+        <div>
+            <h3>Dark Mode</h3>
+        </div>
     </header>
 </template>
 <script setup lang="ts">
 
 </script>
 <style scoped>
+header {
+    background: red;
+}
 </style>
