b4061f3508be7d3528aa4bb8a399d03c7c782111
das972811 <salasdiego54@gmail.com>
2023-07-18 14:33:09 -0500

    Add the 'TheCountries' components and registered the component in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
     <the-header></the-header>
-    <main>{{ count }}</main>
+    <the-countries></the-countries>
     <the-footer></the-footer>
 </template>
 <script setup lang="ts">
     import { onMounted } from 'vue';
     
     import TheHeader from './components/layouts/TheHeader.vue';
+    import TheCountries from './components/layouts/TheCountries.vue';
     import TheFooter from './components/layouts/TheFooter.vue';
-
-    let count = 0;
-    count++;
 
     onMounted(() => {
         const colorTheme = window.matchMedia('(prefers-color-scheme: dark)');
---
+++ src/components/layouts/TheCountries.vue
@@ -0,0 +1,9 @@
+<template>
+    <main>
+        <p>Diego was here</p>
+    </main>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped>
+</style>
