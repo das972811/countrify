452f7a717f0fabe2b03ef345ff1d1a878851f273
das972811 <salasdiego54@gmail.com>
2023-08-05 00:44:47 -0500

    Styled the country grid

--- src/components/countries/LoadedCountries.vue
+++ src/components/countries/LoadedCountries.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="countries-container">
         <div class="country" v-for="country in countries" :key="country.name.official">
-            <img :src="country.flags.svg" alt="A picture of the country flag"/>
+            <div class="country-image" :style="{ 'background-image': 'url(' + country.flags.png + ')', backgroundColor: 'green' }"></div>
             <h3>{{ country.name.official }}</h3>
         </div>
     </div>
@@ -12,19 +12,21 @@
     import { getAllCountries } from '@/utils/restCountriesApim';
 
     const countries = reactive({... await getAllCountries()});
-    console.log(countries)
-
-    // await new Promise<void>(resolve => {
-    //     setTimeout(() => resolve(), 5000);
-    // });
 </script>
 <style scoped>
     .countries-container {
         display: grid;
-        grid-template-columns: auto auto auto auto;
+        justify-content: space-between;
+        text-align: center;
+        grid-template-columns: repeat(4, 16.5rem);
+        grid-template-rows: auto;
+        row-gap: 4rem;
     }
-    img {
-        width: 10rem;
-        height: auto;
+    .countries-container .country-image {
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+        width: 16.5rem;
+        height: 10.5rem;
     }
 </style>
