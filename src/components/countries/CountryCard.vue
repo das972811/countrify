952f75b2e99f68bbbbcf60e6a9ae0cc781ddfafe
das972811 <salasdiego54@gmail.com>
2023-08-11 17:39:59 -0500

    Finished styling on displaying countries

--- src/components/countries/CountryCard.vue
+++ src/components/countries/CountryCard.vue
@@ -2,10 +2,10 @@
     <div class="country">
         <div class="country-image" :style="countrySvgFlag"></div>
         <div class="country-information">
-            <h3>{{ title }}</h3>
-            <p>Population: {{ population }}</p>
-            <p>Region: {{ region }}</p>
-            <p>Capital: {{ capitalFormated }}</p>
+            <h3 class="country-information__title">{{ title }}</h3>
+            <p class="country-information__population">Population: <span class="gray-text">{{ population }}</span></p>
+            <p class="country-information__region">Region: <span class="gray-text">{{ region }}</span></p>
+            <p class="country-information__capital">Capital: <span class="gray-text">{{ capitalFormated }}</span></p>
         </div>
     </div>
 </template>
@@ -40,10 +40,52 @@
 
 </script>
 <style scoped>
-    /* .country {
-    } */
+    .country {
+        text-align: left;
+        position: relative;
+        cursor: pointer;
+    }
 
     .country-information {
         background: var(--accent-color);
+        height: 10.5rem;
+        padding: 1.5rem 2rem  0 1.5rem;
+    }
+
+    .country-information.hide {
+        display: none;
+    }
+
+    .country-information__title {
+        margin-bottom: 0.75rem;
+        font-size: 1.05rem;
+    }
+
+    .country-information > p {
+        margin-bottom: 0.25rem;
+        font-size: 0.84rem;
+    }
+
+    .country-information span.gray-text {
+        color: var(--accent-text-color);
+    }
+
+    /* .country-information__population {
+
+    }
+
+    .country-information__region {
+
+    }
+
+    .country-information__capital {
+
+    } */
+
+    @media screen and (max-width: 650px) {
+        .country-information {
+            width: 20.25rem;
+            margin: 0 auto;
+        }
     }
 </style>
